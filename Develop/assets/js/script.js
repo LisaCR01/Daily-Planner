c4be685cdb7e6d14ee5c424a5721ec1394d79458
Lisa Gunn <lisagunn@Lisas-MacBook-Pro.local>
2022-01-20 19:32:17 +0000

    added on JS to make calendar change colour depending on time of the day

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,8 +1,8 @@
 //displays the current date
 var now =moment().format('dddd, MMMM Do');
 $("#currentDay").text(now);
+var currentHour=moment().format("HH")
 
-//
 const detailTextEl = $('#detail_text');
 const detailFormEl = $('#detail_form');
 //Details written by user into each time block. 
@@ -11,7 +11,6 @@ var myActivity=[];
 // Every time user presses submit handleSubmit function occurs
 const handleSubmit = function (event){
     event.preventDefault();
-    alert("hello")
     // myActivity array
     myActivity[9] = $("#9").val();
     myActivity[10] = $("#10").val();
@@ -31,9 +30,8 @@ function init() {
     // Computer gets stored results from localStorage
     var storedActivity = JSON.parse(localStorage.getItem("myActivity"));
     // If myActivity were retrieved from localStorage, update the myActivity array to it
-    if (storedActivity !== null) {
+    if (storedActivity !== '') {
         myActivity = storedActivity;
-        console.log(myActivity)
         $('textarea[name="t09"]').val(myActivity[9]);
         $('textarea[name="t10"]').val(myActivity[10]);
         $('textarea[name="t11"]').val(myActivity[11]);
@@ -45,6 +43,18 @@ function init() {
         $('textarea[name="t17"]').val(myActivity[17]);
       } 
     }
+
+    $(document).ready(function(){
+    $('.description').each(function(){
+        var id = $(this).attr('id')
+        var i = parseInt(id.substring(1));
+        console.log(i)
+        console.log(i-currentHour);
+        if(i<currentHour){$(this).addClass("past")}
+        if(i==currentHour){$(this).addClass("present")}
+        if(i>currentHour){$(this).addClass("future")}
+              });
+          });
 
     detailFormEl.on("submit",handleSubmit);
 
