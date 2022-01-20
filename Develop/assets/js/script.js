688ccfdf51887eec74925b408f7ac281d87da89e
Lisa Gunn <lisagunn@Lisas-MacBook-Pro.local>
2022-01-20 16:40:26 +0000

    added local storage

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,19 +1,54 @@
+//displays the current date
 var now =moment().format('dddd, MMMM Do');
 $("#currentDay").text(now);
 
+//
 const detailTextEl = $('#detail_text');
 const detailFormEl = $('#detail_form');
+//Details written by user into each time block. 
+var myActivity=[];
 
+// Every time user presses submit handleSubmit function occurs
 const handleSubmit = function (event){
     event.preventDefault();
-    const detailItem = detailTextEl.val()
-    if(detailItem !==''){
-        const storedDetails =$('<textarea>').text(detailItem)
+    alert("hello")
+    // myActivity array
+    myActivity[9] = $("#9").val();
+    myActivity[10] = $("#10").val();
+    myActivity[11] = $("#11").val();
+    myActivity[12] = $("#9").val();
+    myActivity[13] = $("#11").val();
+    myActivity[14] = $("#12").val();
+    myActivity[15] = $("#13").val();
+    myActivity[16] = $("#14").val();
+    myActivity[17] = $("#15").val();
+    // Stringify and set key in localStorage to myActivity array
+    localStorage.setItem("myActivity", JSON.stringify(myActivity));
+}
+
+// Computer calls will run this function when the page loads. 
+function init() {
+    // Computer gets stored results from localStorage
+    var storedActivity = JSON.parse(localStorage.getItem("myActivity"));
+    // If myActivity were retrieved from localStorage, update the myActivity array to it
+    if (storedActivity !== null) {
+        myActivity = storedActivity;
+        console.log(myActivity)
+        $('textarea[name="t09"]').val(myActivity[9]);
+        $('textarea[name="t10"]').val(myActivity[10]);
+        $('textarea[name="t11"]').val(myActivity[11]);
+        $('textarea[name="t12"]').val(myActivity[12]);
+        $('textarea[name="t13"]').val(myActivity[13]);
+        $('textarea[name="t14"]').val(myActivity[14]);
+        $('textarea[name="t15"]').val(myActivity[15]);
+        $('textarea[name="t16"]').val(myActivity[16]);
+        $('textarea[name="t17"]').val(myActivity[17]);
+      } 
     }
 
-};
-detailFormEl.on("submit",handleSubmit)
+    detailFormEl.on("submit",handleSubmit);
 
+    init()
 
 
   
